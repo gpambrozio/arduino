8e7170b6653acd82f7ce14d941bd14998ba34c9b
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2014-07-25 01:27:31 -0700

    Sprinkler controller is done! (v1 that is...)

--- SprinklerController/SprinklerController.ino
+++ SprinklerController/SprinklerController.ino
@@ -39,17 +39,22 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
 
+#define START_HOUR        2
+
+#define DAYS_PERIOD_R1    2
+#define DAYS_PERIOD_R2    3
+#define MINUTES_R1       30
+#define MINUTES_R2       30
+
 #define clockAddress 0x68
 
 #define RELAY1   10
 #define RELAY2   11
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(0, 2, 6, 7, 8, 9);
+LiquidCrystal lcd(3, 2, 6, 7, 8, 9);
 
-long previousMillis = 0;  // will store last time Temp was updated
 byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
-byte test; 
 
 // Convert normal decimal numbers to binary coded decimal
 byte decToBcd(byte val)
@@ -88,6 +93,7 @@ void setDateDs1307()
   Wire.write(decToBcd(dayOfMonth));
   Wire.write(decToBcd(month));
   Wire.write(decToBcd(year));
+  Wire.write(byte(0x00));
   Wire.endTransmission();
 }
 
@@ -124,12 +130,46 @@ void getDateDs1307() {
   Serial.print(year, DEC);
 }
 
+void twoDigit(byte x) {
+  if (x < 10)
+    lcd.print("0");
+  lcd.print(x);
+}
+
+int daysFromStartOfYear(byte day, byte month) {
+  switch(month) {
+    case 1:
+      return day;
+    case 2:
+      return 31+day;
+    case 3:
+      return 59+day;
+    case 4:
+      return 90+day;
+    case 5:
+      return 120+day;
+    case 6:
+      return 151+day;
+    case 7:
+      return 181+day;
+    case 8:
+      return 212+day;
+    case 9:
+      return 243+day;
+    case 10:
+      return 273+day;
+    case 11:
+      return 304+day;
+    case 12:
+      return 334+day;
+  }
+}
 
 void setup() {
-  pinMode(RELAY1, OUTPUT);
-  pinMode(RELAY2, OUTPUT);
   digitalWrite(RELAY1, LOW);
   digitalWrite(RELAY2, LOW);
+  pinMode(RELAY1, OUTPUT);
+  pinMode(RELAY2, OUTPUT);
   
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
@@ -143,70 +183,105 @@ void setup() {
 }
 
 int command = 0;
+int aux1, aux2;
+
+byte forceR1 = 0;
+byte forceR2 = 0;
 
 void loop() {
   if (Serial.available()) {  // Look for char in serial que and process if found
     command = Serial.read();
-    if (command == 'T') {      //If command = "T" Set Date
-      setDateDs1307();
-      getDateDs1307();
-      Serial.println("");
-    }
-    else if (command == 'Q') {  //If command = "Q" RTC1307 Memory Functions
-      delay(100);
-      if (Serial.available()) {
-        command = Serial.read(); 
-        
-        // If command = "1" RTC1307 Initialize Memory - All Data will be set to 255 (0xff).  
-        // Therefore 255 or 0 will be an invalid value.  
-        if (command == '1') { 
-          
-          // 255 will be the init value and 0 will be cosidered an error that 
-          // occurs when the RTC is in Battery mode. 
-          Wire.beginTransmission(clockAddress);
-          
-          // Set the register pointer to be just past the date/time registers.
-          Wire.write(byte(0x08));  
-          for (int i = 1; i <= 27; i++) {
-            Wire.write(byte(0xff));
-            delay(100);
-          }   
-          Wire.endTransmission();
-          getDateDs1307();
-          Serial.println(": RTC1307 Initialized Memory");
-        }
-        else if (command == '2') {      //If command = "2" RTC1307 Memory Dump
-          getDateDs1307();
-          Serial.println(": RTC 1307 Dump Begin");
-          Wire.beginTransmission(clockAddress);
-          Wire.write(byte(0x00));
-          Wire.endTransmission();
-          Wire.requestFrom(clockAddress, 64);
-          for (int i = 1; i <= 64; i++) {
-            test = Wire.read();
-            Serial.print(i);
-            Serial.print(":");
-            Serial.println(test, DEC);
-          }
-          Serial.println(" RTC1307 Dump end");
-        } 
-      }  
-    }
     Serial.print("Command: ");
     Serial.println(command);  // Echo command CHAR in ascii that was sent
+    switch (command) {
+      case 'T':      //If command = "T" Set Date
+        setDateDs1307();
+        getDateDs1307();
+        Serial.println("");
+        break;
+
+      case 'R':
+        aux1 = Serial.read();
+        aux2 = Serial.read();
+        if (aux1 == '1') {
+          forceR1 = aux2 == '1' ? 1 : aux2 == '0' ? 2 : 0;
+        } else if (aux1 == '2') {
+          forceR2 = aux2 == '1' ? 1 : aux2 == '0' ? 2 : 0;
+        }
+        break;
+    }
   }
 
   command = 0;  // reset command                  
   delay(100);
 
+  getDateDs1307();
+  Serial.println("");
+
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 0);
+  switch(dayOfWeek) {
+    case 1:
+      lcd.print("Sun");
+      break;
+    case 2:
+      lcd.print("Mon");
+      break;
+    case 3:
+      lcd.print("Tue");
+      break;
+    case 4:
+      lcd.print("Wed");
+      break;
+    case 5:
+      lcd.print("Thu");
+      break;
+    case 6:
+      lcd.print("Fri");
+      break;
+    case 7:
+      lcd.print("Sat");
+      break;
+    default:
+      lcd.print("??");
+      lcd.print(dayOfWeek);
+      break;
+  }
+  lcd.print(" ");
+  twoDigit(month);
+  lcd.print("/");
+  twoDigit(dayOfMonth);
+  lcd.print("/");
+  twoDigit(year);
+  lcd.print(" ");
+  twoDigit(daysFromStartOfYear(dayOfMonth, month));
+
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(hour);
+  twoDigit(hour);
   lcd.print(":");
-  lcd.print(minute);
+  twoDigit(minute);
   lcd.print(":");
-  lcd.print(second);
+  twoDigit(second);
+  lcd.print(" ");
+  
+  if (forceR1 == 1 || (forceR1 == 0 && hour == START_HOUR && minute <= MINUTES_R1 &&
+      (daysFromStartOfYear(dayOfMonth, month) % DAYS_PERIOD_R1) == 0)) {
+    digitalWrite(RELAY1, HIGH);
+    lcd.print("R1");
+  } else {
+    digitalWrite(RELAY1, LOW);
+    lcd.print("--");
+  }
+  lcd.print(" ");
+  if (forceR2 == 1 || (forceR2 == 0 && hour == START_HOUR && minute <= MINUTES_R2 &&
+      (daysFromStartOfYear(dayOfMonth, month) % DAYS_PERIOD_R2) == 0)) {
+    digitalWrite(RELAY2, HIGH);
+    lcd.print("R2");
+  } else {
+    digitalWrite(RELAY2, LOW);
+    lcd.print("--");
+  }
 }
 
