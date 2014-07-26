329451a0d002db13ca0e24fea54e8d8902225acb
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2014-07-26 02:19:17 -0700

    Some simplifications to the sprinkler controller

--- SprinklerController/SprinklerController.ino
+++ SprinklerController/SprinklerController.ino
@@ -1,38 +1,17 @@
 /*
-  LiquidCrystal Library - Hello World
- 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the 
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD
- and shows the time.
  
   The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
+ * LCD RS pin to digital pin 3
+ * LCD Enable pin to digital pin 2
+ * LCD D4 pin to digital pin 6
+ * LCD D5 pin to digital pin 7
+ * LCD D6 pin to digital pin 8
+ * LCD D7 pin to digital pin 9
  * LCD R/W pin to ground
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
  
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- 
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
 
 // include the library code:
@@ -116,7 +95,9 @@ void getDateDs1307() {
   dayOfMonth = bcdToDec(Wire.read());
   month      = bcdToDec(Wire.read());
   year       = bcdToDec(Wire.read());
+}
 
+void printDateToSerial() {
   Serial.print(hour, DEC);
   Serial.print(":");
   Serial.print(minute, DEC);
@@ -128,7 +109,7 @@ void getDateDs1307() {
   Serial.print(dayOfMonth, DEC);
   Serial.print("/");
   Serial.print(year, DEC);
-}
+}  
 
 void twoDigit(byte x) {
   if (x < 10)
