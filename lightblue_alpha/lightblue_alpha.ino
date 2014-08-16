bfb9529242be953d3ac15c264bbd251be88d5a8d
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2014-08-16 03:39:21 -0700

    New project for the bean to write to the serial display

---
+++ lightblue_alpha/lightblue_alpha.ino
@@ -0,0 +1,103 @@
+// Demo the quad alphanumeric display LED backpack kit
+// scrolls through every character, then scrolls Serial
+// input onto the display
+
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+#define LOOP_SLEEP       300
+#define LOOP_LONG_SLEEP  6000
+
+Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
+
+void setup() {
+  Serial.begin(57600);
+  
+  alpha4.begin(0x70);  // pass in the address
+
+  alpha4.writeDigitRaw(3, 0x0);
+  alpha4.writeDigitRaw(0, 0xFFFF);
+  alpha4.writeDisplay();
+  delay(200);
+  alpha4.writeDigitRaw(0, 0x0);
+  alpha4.writeDigitRaw(1, 0xFFFF);
+  alpha4.writeDisplay();
+  delay(200);
+  alpha4.writeDigitRaw(1, 0x0);
+  alpha4.writeDigitRaw(2, 0xFFFF);
+  alpha4.writeDisplay();
+  delay(200);
+  alpha4.writeDigitRaw(2, 0x0);
+  alpha4.writeDigitRaw(3, 0xFFFF);
+  alpha4.writeDisplay();
+  delay(200);
+}
+
+bool compareScratch( ScratchData * scratch1, ScratchData * scratch2 ) {
+  bool matched = true;
+  
+  if ( scratch1->length != scratch2->length )
+  {
+    matched = false;
+  }
+  else
+  {
+    int length = min( scratch1->length, scratch2->length );
+    int i = 0;
+    
+    while ( i < length )
+    {
+      if ( scratch1->data[i] != scratch2->data[i] )
+      {
+        matched = false;
+        i = length;
+      }
+      i++;
+    }
+  }
+  
+  return matched;
+}
+ 
+ScratchData lastScratch;
+int8_t displayPosition = 0;
+uint8_t stringSize = 0;
+uint32_t loopNumber = 0;
+uint32_t stopDisplay = 0;
+
+void loop() {
+  ScratchData thisScratch = Bean.readScratchData(1);
+  bool matched = compareScratch( &thisScratch, &lastScratch );
+  
+  if ( !matched ) {
+    lastScratch = thisScratch;
+    displayPosition = -3;
+    stringSize = lastScratch.length;
+    stopDisplay = loopNumber + (10000 / LOOP_SLEEP);
+  }
+
+  if (loopNumber == stopDisplay) {
+    alpha4.clear();
+    alpha4.writeDisplay();
+  } else if (loopNumber < stopDisplay) {
+    // scroll down display
+    for (uint8_t i=0;i<4;i++) {
+      int8_t pos = displayPosition + i;
+      if (pos >= 0 && pos < stringSize) {
+        alpha4.writeDigitAscii(i, lastScratch.data[pos]);
+      } else {
+        alpha4.writeDigitAscii(i, ' ');
+      }
+    }
+    
+    if (++displayPosition > stringSize) {
+      displayPosition = -3;
+    }
+   
+    alpha4.writeDisplay();
+  }
+  
+  Bean.sleep(LOOP_SLEEP);
+  ++loopNumber;
+}
