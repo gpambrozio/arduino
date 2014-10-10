7ab883a4633b25e9f6dd8b01283a2f3d96c33c02
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2014-10-10 00:36:45 -0700

    Added RTC and made it time aware.

--- ExploMobile/ExploMobile.ino
+++ ExploMobile/ExploMobile.ino
@@ -2,6 +2,8 @@
 */
 
 #include <SPI.h>
+#include <Wire.h>
+
 #include "RadioCommon.h"
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -24,14 +26,97 @@ byte modes[] = {0, 47, 120, 170, 240};
 // First = CE. Second = CS
 RF24 radio(2, 10);
 
+#define clockAddress 0x68
+
+byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+byte lastReportedMinute = 100;
+
+byte decToBcd(byte val)
+{
+  return ( (val/10*16) + (val%10) );
+}
+
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+  return ( (val/16*10) + (val%16) );
+}
+
+// 1) Sets the date and time on the ds1307
+// 2) Starts the clock
+// 3) Sets hour mode to 24 hour clock
+// Assumes you're passing in valid numbers, 
+// Probably need to put in checks for valid numbers.
+void setDateDs1307()                
+{
+  // Use of (byte) type casting and ascii math to achieve result.  
+  second = (byte) ((Serial.read() - '0') * 10 + (Serial.read() - '0')); 
+  minute = (byte) ((Serial.read() - '0') *10 +  (Serial.read() - '0'));
+  hour  = (byte) ((Serial.read() - '0') *10 +  (Serial.read() - '0'));
+  dayOfWeek = (byte) (Serial.read() - '0');
+  dayOfMonth = (byte) ((Serial.read() - '0') *10 +  (Serial.read() - '0'));
+  month = (byte) ((Serial.read() - '0') *10 +  (Serial.read() - '0'));
+  year= (byte) ((Serial.read() - '0') *10 +  (Serial.read() - '0'));
+  Wire.beginTransmission(clockAddress);
+  Wire.write(byte(0x00));
+  Wire.write(decToBcd(second));  // 0 to bit 7 starts the clock
+  Wire.write(decToBcd(minute));
+  Wire.write(decToBcd(hour));    // If you want 12 hour am/pm you need to set
+  // bit 6 (also need to change readDateDs1307)
+  Wire.write(decToBcd(dayOfWeek));
+  Wire.write(decToBcd(dayOfMonth));
+  Wire.write(decToBcd(month));
+  Wire.write(decToBcd(year));
+  Wire.write(byte(0x00));
+  Wire.endTransmission();
+}
+
+// Gets the date and time from the ds1307 and prints result
+void getDateDs1307() {
+  // Reset the register pointer
+  Wire.beginTransmission(clockAddress);
+  Wire.write(byte(0x00));
+  Wire.endTransmission();
+
+  Wire.requestFrom(clockAddress, 7);
+
+  // A few of these need masks because certain bits are control bits
+  second     = bcdToDec(Wire.read() & 0x7f);
+  minute     = bcdToDec(Wire.read());
+  
+  // Need to change this if 12 hour am/pm
+  hour       = bcdToDec(Wire.read() & 0x3f);  
+  dayOfWeek  = bcdToDec(Wire.read());
+  dayOfMonth = bcdToDec(Wire.read());
+  month      = bcdToDec(Wire.read());
+  year       = bcdToDec(Wire.read());
+}
+
+void printDateToSerial() {
+  Serial.print(hour, DEC);
+  Serial.print(":");
+  Serial.print(minute, DEC);
+  Serial.print(":");
+  Serial.print(second, DEC);
+  Serial.print("  ");
+  Serial.print(month, DEC);
+  Serial.print("/");
+  Serial.print(dayOfMonth, DEC);
+  Serial.print("/");
+  Serial.println(year, DEC);
+}
+
 void setup() {
   digitalWrite(MOTOR, LOW);
   pinMode(MOTOR, OUTPUT);
 
+  Wire.begin();
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Starting"); 
 
   START_RADIO(radio, RADIO_MOBILE);
+
+  startBananas();
 }
 
 int motorPower = MOTOR_MIN;
@@ -43,16 +128,19 @@ int mode = -1;
 boolean bananas = false;
 long bananasTargetTime;
 
+long offTargetTime;
 long rampTargetTime;
 long rampStartTime;
 byte rampStartMotor;
 byte rampTargetMotor;
 double rampMotorDelta;
 
+boolean sleeping = false;
+
 #define DEFAULT_RAMP    20    // Meaning this change in power per second
 
-#define SPEED_UP    { rampTargetTime = 0; mode = -1; bananas = false; if (motorPower == 0) motorPower = MOTOR_MIN; else if (motorPower < MOTOR_MAX) motorPower++; Serial.println(motorPower); }
-#define SPEED_DOWN  { rampTargetTime = 0; mode = -1; bananas = false; if (motorPower > MOTOR_MIN) motorPower--; else motorPower = 0; Serial.println(motorPower); };
+#define SPEED_UP    { rampTargetTime = 0; mode = -1; bananas = false; if (motorPower == 0) motorPower = MOTOR_MIN; else if (motorPower < MOTOR_MAX) motorPower++; rampTargetMotor = motorPower; Serial.println(motorPower); }
+#define SPEED_DOWN  { rampTargetTime = 0; mode = -1; bananas = false; if (motorPower > MOTOR_MIN) motorPower--; else motorPower = 0; rampTargetMotor = motorPower; Serial.println(motorPower); };
 
 long rampChange(int motor) {
   if (motor == motorPower) {
@@ -91,6 +179,29 @@ void startBananas() {
 }
 
 void loop() {
+  getDateDs1307();
+  if (lastReportedMinute != minute) {
+    printDateToSerial();
+    lastReportedMinute = minute;
+  }
+  
+  if ((hour >= 21 || hour < 8) && offTargetTime == 0 && motorPower != 0 && rampTargetMotor != 0) {
+    offTargetTime = millis() + 5 * 60000;
+  }
+  
+  if (offTargetTime > 0 && millis() > offTargetTime) {
+    offTargetTime = 0;
+    bananas = false;
+    mode = -1;
+    rampChange(0);
+    sleeping = true;
+  }
+  
+  if (sleeping && hour >= 8) {
+    sleeping = false;
+    startBananas();
+  }
+  
   if (bananas && millis() > bananasTargetTime) {
     bananasChange();
   }
@@ -152,6 +263,37 @@ void loop() {
   } else if (buttonState & B1100) {   // Only repeat for button A and B
     if (millis() - debounce > 50) {
       buttonState = 0;
+    }
+  }
+
+  if (Serial.available()) {  // Look for char in serial que and process if found
+    int command = Serial.read();
+    Serial.print("Command: ");
+    Serial.println(command);  // Echo command CHAR in ascii that was sent
+    switch (command) {
+      case 'T':      //If command = "T" Set Date
+        setDateDs1307();
+        getDateDs1307();
+        printDateToSerial();
+        break;
+
+      case '+':
+        Serial.println("Received +");
+        SPEED_UP
+        break;
+        
+      case '-':
+        Serial.println("Received -");
+        SPEED_DOWN
+        break;
+        
+      case 'B':
+        startBananas();
+        break;
+
+      default:
+        Serial.print("Received command ");
+        Serial.println(command);
     }
   }
 
