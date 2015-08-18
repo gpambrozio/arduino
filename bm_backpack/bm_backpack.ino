6accad90f11526b4addfbcdc3ec22e87c5c4027f
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2015-08-18 01:59:27 -0700

    Made the backpack sound analysis really simple.

--- bm_backpack/bm_backpack.ino
+++ bm_backpack/bm_backpack.ino
@@ -19,97 +19,16 @@ Connections:
 Written by Adafruit Industries.  Distributed under the BSD license --
 see license.txt for more information.  This paragraph must be included
 in any redistribution.
-
-ffft library is provided under its own terms -- see ffft.S for specifics.
 */
 
-// IMPORTANT: FFT_N should be #defined as 128 in ffft.h.  This is different
-// than Spectro, which requires FFT_N be 64 in that file when compiling.
-
-#include <avr/pgmspace.h>
-#include <ffft.h>
-#include <math.h>
-#include <Wire.h>
 #include <SPI.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_WS2801.h>
 
-// Microphone connects to Analog Pin 0.  Corresponding ADC channel number
-// varies among boards...it's ADC0 on Uno and Mega, ADC7 on Leonardo.
-// Other boards may require different settings; refer to datasheet.
-#ifdef __AVR_ATmega32U4__
- #define ADC_CHANNEL 7
-#else
- #define ADC_CHANNEL 2
-#endif
+#define MIC_PIN      A2  // Microphone is attached to this analog pin
 
 #define AGC_CONTROL  17
 
-
-int16_t       capture[FFT_N];    // Audio capture buffer
-complex_t     bfly_buff[FFT_N];  // FFT "butterfly" buffer
-uint16_t      spectrum[FFT_N/2]; // Spectrum output buffer
-volatile byte samplePos = 0;     // Buffer position counter
-
-byte
-  colCount = 0; // Frame counter for storing past column data
-int
-  col[8][10],   // Column levels for the prior 10 frames
-  minLvlAvg[8], // For dynamic adjustment of low & high ends of graph,
-  maxLvlAvg[8], // pseudo rolling averages for the prior few frames.
-  colDiv[8];    // Used when filtering FFT output to 8 columns
-
-/*
-These tables were arrived at through testing, modeling and trial and error,
-exposing the unit to assorted music and sounds.  But there's no One Perfect
-EQ Setting to Rule Them All, and the graph may respond better to some
-inputs than others.  The software works at making the graph interesting,
-but some columns will always be less lively than others, especially
-comparing live speech against ambient music of varying genres.
-*/
- 
-  // This is low-level noise that's subtracted from each FFT output column:
-uint8_t const noise[64] PROGMEM = { 8,6,6,5,3,4,4,4,3,4,4,3,2,3,3,4,
-              2,1,2,1,3,2,3,2,1,2,3,1,2,3,4,4,
-              3,2,2,2,2,2,2,1,3,2,2,2,2,2,2,2,
-              2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,4 };
-  // These are scaling quotients for each FFT output column, sort of a
-  // graphic EQ in reverse.  Most music is pretty heavy at the bass end.
-uint8_t const eq[64] PROGMEM = {
-    255, 175,218,225,220,198,147, 99, 68, 47, 33, 22, 14,  8,  4,  2,
-      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 };
-  // When filtering down to 8 columns, these tables contain indexes
-  // and weightings of the FFT spectrum output values to use.  Not all
-  // buckets are used -- the bottom-most and several at the top are
-  // either noisy or out of range or generally not good for a graph.
-uint8_t const col0data[] PROGMEM = {  2,  1,  // # of spectrum bins to merge, index of first
-    111,   8 },           // Weights for each bin
-  col1data[] = {  4,  1,  // 4 bins, starting at index 1
-     19, 186,  38,   2 }, // Weights for 4 bins.  Got it now?
-  col2data[] = {  5,  2,
-     11, 156, 118,  16,   1 },
-  col3data[] = {  8,  3,
-      5,  55, 165, 164,  71,  18,   4,   1 },
-  col4data[] = { 11,  5,
-      3,  24,  89, 169, 178, 118,  54,  20,   6,   2,   1 },
-  col5data[] = { 17,  7,
-      2,   9,  29,  70, 125, 172, 185, 162, 118, 74,
-     41,  21,  10,   5,   2,   1,   1 },
-  col6data[] = { 25, 11,
-      1,   4,  11,  25,  49,  83, 121, 156, 180, 185,
-    174, 149, 118,  87,  60,  40,  25,  16,  10,   6,
-      4,   2,   1,   1,   1 },
-  col7data[] = { 37, 16,
-      1,   2,   5,  10,  18,  30,  46,  67,  92, 118,
-    143, 164, 179, 185, 184, 174, 158, 139, 118,  97,
-     77,  60,  45,  34,  25,  18,  13,   9,   7,   5,
-      3,   2,   2,   1,   1,   1,   1 },
-  // And then this points to the start of the data for each of the columns:
-  *colData[] = {
-    col0data, col1data, col2data, col3data,
-    col4data, col5data, col6data, col7data };
+#define TOP   6  // A bit more to go off scale.
 
 #define DATA_PIN  10   // Yellow
 #define CLOCK_PIN 11   // Green
@@ -117,122 +36,85 @@ uint8_t const col0data[] PROGMEM = {  2,  1,  // # of spectrum bins to merge, in
 // Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
 Adafruit_WS2801 strip = Adafruit_WS2801(25, DATA_PIN, CLOCK_PIN);
 
+// MUSIC STUFF  ---------------------------------------------------------
+#define DC_OFFSET  0  // DC offset in mic signal - if unusure, leave 0
+#define NOISE     10  // Noise/hum/interference in mic signal
+#define SAMPLES   12  // Length of buffer for dynamic level adjustment
+
+int vol[SAMPLES];
+
+uint8_t volCount;    // Frame counter for storing past volume data
+uint8_t currentColumn;
+
+int lvl;          // Current "dampened" audio level
+int minLvlAvg;    // For dynamic adjustment of graph low & high
+int maxLvlAvg;
+int height;
+
+uint16_t minLvl;
+uint16_t maxLvl;
+
 void setup() {
-  uint8_t i, j, nBins, binNum, *data;
-
-  memset(col , 0, sizeof(col));
-
-  for(i=0; i<8; i++) {
-    minLvlAvg[i] = 0;
-    maxLvlAvg[i] = 512;
-    data         = (uint8_t *)pgm_read_word(&colData[i]);
-    nBins        = pgm_read_byte(&data[0]) + 2;
-    binNum       = pgm_read_byte(&data[1]);
-    for(colDiv[i]=0, j=2; j<nBins; j++)
-      colDiv[i] += pgm_read_byte(&data[j]);
-  }
-
   pinMode(AGC_CONTROL, INPUT);
 
   strip.begin();
   strip.show();
 
-  // Init ADC free-run mode; f = ( 16MHz/prescaler ) / 13 cycles/conversion 
-#if ADC_CHANNEL < 8
-  ADMUX  = _BV(REFS0) | ADC_CHANNEL; // Channel sel, right-adj, use AREF pin
-#else
-  ADMUX  = _BV(REFS0) | (ADC_CHANNEL-8); // Channel sel, right-adj, use AREF pin
-#endif
-  ADCSRA = _BV(ADEN)  | // ADC enable
-  _BV(ADSC)  | // ADC start
-  _BV(ADATE) | // Auto trigger
-  _BV(ADIE)  | // Interrupt enable
-  _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // 128:1 / 13 = 9615 Hz
-#if ADC_CHANNEL < 8
-  ADCSRB = 0;                // Free run mode, no high MUX bit
-  DIDR0  = 1 << ADC_CHANNEL; // Turn off digital input for ADC pin
-#else
-  ADCSRB = B100000;
-  DIDR2  = 1 << (ADC_CHANNEL-8);
-#endif
-#ifdef TIMSK0
-  TIMSK0 = 0;                // Timer0 off
-#else
-  TIMSK = 0;                // Timer0 off
-#endif
-
-  sei(); // Enable interrupts
+  memset(vol, 0, sizeof(int) * SAMPLES);
 }
 
 void drawLine(uint16_t x, uint16_t height, uint32_t color) {
   for (int i=0;i<5;i++) {
     if (i<height) {
-      strip.setPixelColor((x % 2 == 1 ? (4 - i) : i) + x * 5, color);
-    } else {
       strip.setPixelColor((x % 2 == 1 ? (4 - i) : i) + x * 5, 0);
+    } else {
+      strip.setPixelColor((x % 2 == 1 ? (4 - i) : i) + x * 5, color);
     }
   }
+  strip.show();
 }
 
+int i;
+
 void loop() {
-  uint8_t  i, x, L, *data, nBins, binNum, weighting, c;
-  uint16_t minLvl, maxLvl;
-  int      level, y, sum;
 
-  while(ADCSRA & _BV(ADIE)); // Wait for audio sampling to finish
+  height   = analogRead(MIC_PIN);                        // Raw reading from mic
+  height   = abs(height - 512 - DC_OFFSET); // Center on zero
+  height   = (height <= NOISE) ? 0 : (height - NOISE);             // Remove noise/hum
+  lvl = ((lvl * 7) + height) >> 3;    // "Dampened" reading (else looks twitchy)
 
-  fft_input(capture, bfly_buff);   // Samples -> complex #s
-  samplePos = 0;                   // Reset sample counter
-  ADCSRA |= _BV(ADIE);             // Resume sampling interrupt
-  fft_execute(bfly_buff);          // Process complex data
-  fft_output(bfly_buff, spectrum); // Complex -> spectrum
+  vol[volCount] = height;                 // Save sample for dynamic leveling
+  if(++volCount >= SAMPLES) volCount = 0; // Advance/rollover sample counter
+ 
+  // Calculate bar height based on dynamic min/max levels (fixed point):
+  height = TOP * (lvl - minLvlAvg) / (long)(maxLvlAvg - minLvlAvg);
 
-  // Remove noise and apply EQ levels
-  for(x=0; x<FFT_N/2; x++) {
-    L = pgm_read_byte(&noise[x]);
-    spectrum[x] = (spectrum[x] <= L) ? 0 :
-      (((spectrum[x] - L) * (256L - pgm_read_byte(&eq[x]))) >> 8);
+  if(height < 0L)       height = 0;      // Clip output
+  else if(height > TOP) height = TOP;
+
+  // Color pixels based on rainbow gradient
+  drawLine(currentColumn, height, Wheel(map(height,0,TOP,30,150)));
+  if (++currentColumn >= 5) currentColumn = 0;
+
+  // Get volume range of prior frames
+  minLvl = maxLvl = vol[0];
+
+  for(i=1; i<SAMPLES; i++) {
+    if(vol[i] < minLvl)      minLvl = vol[i];
+    else if(vol[i] > maxLvl) maxLvl = vol[i];
   }
 
-  // Downsample spectrum output to 8 columns:
-  for(x=2; x<7; x++) {
-    data   = (uint8_t *)pgm_read_word(&colData[x]);
-    nBins  = pgm_read_byte(&data[0]) + 2;
-    binNum = pgm_read_byte(&data[1]);
-    for(sum=0, i=2; i<nBins; i++)
-      sum += spectrum[binNum++] * pgm_read_byte(&data[i]); // Weighted
-    col[x][colCount] = sum / colDiv[x];                    // Average
-    minLvl = maxLvl = col[x][0];
-    for(i=1; i<10; i++) { // Get range of prior 10 frames
-      if(col[x][i] < minLvl)      minLvl = col[x][i];
-      else if(col[x][i] > maxLvl) maxLvl = col[x][i];
-    }
-    // minLvl and maxLvl indicate the extents of the FFT output, used
-    // for vertically scaling the output graph (so it looks interesting
-    // regardless of volume level).  If they're too close together though
-    // (e.g. at very low volume levels) the graph becomes super coarse
-    // and 'jumpy'...so keep some minimum distance between them (this
-    // also lets the graph go to zero when no sound is playing):
-    if((maxLvl - minLvl) < 10) maxLvl = minLvl + 10;
-    minLvlAvg[x] = (minLvlAvg[x] * 7 + minLvl) >> 3; // Dampen min/max levels
-    maxLvlAvg[x] = (maxLvlAvg[x] * 7 + maxLvl) >> 3; // (fake rolling average)
-
-    // Second fixed-point scale based on dynamic min/max levels:
-    level = 255L * (col[x][colCount] - minLvlAvg[x]) /
-      (long)(maxLvlAvg[x] - minLvlAvg[x]);
-
-    // Clip output and convert to byte:
-    if(level < 0L)        c = 0;
-    else if(level > 255)  c = 255; // Allow dot to go a couple pixels off top
-    else                  c = (uint8_t)level;
-
-    drawLine(x-2, map(c, 0, 255, 0, 7), Wheel(c));
-
-  }
-
-  strip.show();
-
-  if(++colCount >= 10) colCount = 0;
+  // minLvl and maxLvl indicate the volume range over prior frames, used
+  // for vertically scaling the output graph (so it looks interesting
+  // regardless of volume level).  If they're too close together though
+  // (e.g. at very low volume levels) the graph becomes super coarse
+  // and 'jumpy'...so keep some minimum distance between them (this
+  // also lets the graph go to zero when no sound is playing):
+  if((maxLvl - minLvl) < TOP) maxLvl = minLvl + TOP;
+  minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6; // Dampen min/max levels
+  maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6; // (fake rolling average)
+  
+  delay(2);
 }
 
 // Create a 24 bit color value from R,G,B
@@ -260,17 +142,5 @@ uint32_t Wheel(byte WheelPos)
    WheelPos -= 170; 
    return Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
-}
-
-ISR(ADC_vect) { // Audio-sampling interrupt
-  static const int16_t noiseThreshold = 4;
-  int16_t              sample         = ADC; // 0-1023
-
-  capture[samplePos] =
-    ((sample > (256-noiseThreshold)) &&
-     (sample < (256+noiseThreshold))) ? 0 :
-    sample - 256; // Sign-convert for FFT; -512 to +511
-
-  if(++samplePos >= FFT_N) ADCSRA &= ~_BV(ADIE); // Buffer full, interrupt off
 }
 
