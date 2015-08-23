322b578d15f93d774dc6bc805ece43860b0b9a98
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2015-08-23 01:16:44 -0700

    Big improvement to Nancy's coat. Now has multiple modes and the final circuit.

--- nancy_coat/nancy_coat.ino
+++ nancy_coat/nancy_coat.ino
@@ -1,7 +1,11 @@
 #include <Adafruit_NeoPixel.h>
+#include <EEPROM.h>
 #include "SimpleVolume.h"
 
-#define PIN 6
+#define LED_PIN     2
+#define MIC_PIN     A1  // Microphone is attached to this analog pin
+#define GAIN_PIN    A0
+#define EXTRA_GND   13
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -10,7 +14,7 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(39, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(39, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -19,46 +23,96 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(39, PIN, NEO_GRB + NEO_KHZ800);
 
 // MUSIC STUFF  ---------------------------------------------------------
 #define SAMPLES    60   // Length of buffer for dynamic level adjustment
-#define MIC_PIN    A0  // Microphone is attached to this analog pin
 #define TOP        100
+#define BAR_LENGTH 4
 
 SimpleVolume sv = SimpleVolume(MIC_PIN, SAMPLES, TOP, 270);
 
+typedef enum {
+  modeSound = 0,
+  modeSoundBar,
+  modeRainbow,
+  modeTheaterChaseRainbow,
+  modeTotal
+} mode;
+
+byte currentMode;
+
 void setup() {
+  pinMode(EXTRA_GND, OUTPUT);
+  digitalWrite(EXTRA_GND, LOW);
+  
+  // Floating = 60dB
+  // Low = 50dB
+  // High = 40dB
+  pinMode(GAIN_PIN, INPUT);
+  
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   
   Serial.begin(9600);
   sv.start();
+  
+  currentMode = EEPROM.read(0);
+  EEPROM.write(0, (currentMode + 1 >= modeTotal) ? 0 : currentMode + 1);
 }
 
 uint16_t currentCycle = 0;
 uint8_t cycleCount = 0;
 
 void loop() {  
-  /*
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127,   0,   0), 50); // Red
-  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
-
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
-  */
-
-  int height = sv.getVolume();
-  strip.setBrightness(height);
-  if (++cycleCount > 10) {
-    if (++currentCycle >= 256*5) currentCycle = 0;
-    cycleCount = 0;
+  switch (currentMode) {
+    case modeSound:
+    {
+      int height = sv.getVolume();
+      strip.setBrightness(height);
+      if (++cycleCount > 10) {
+        if (++currentCycle >= 256*5) currentCycle = 0;
+        cycleCount = 0;
+      }
+      rainbowCycleStep(currentCycle);
+      break;
+    } 
+    
+    case modeSoundBar:
+    {
+      strip.setBrightness(TOP);
+      int height = sv.getVolume();
+      
+      int barHeight = map(height, 0, TOP, 0, BAR_LENGTH);
+      for (int i=0;i<BAR_LENGTH;i++) {
+        uint32_t color = 0;
+        if (barHeight >= i) {
+          color = Wheel(height);
+        }
+        strip.setPixelColor(BAR_LENGTH - i - 1, color);
+        strip.setPixelColor(strip.numPixels() - BAR_LENGTH + i, color);
+      }
+      
+      int centerBar = (strip.numPixels() + 1) / 2 - BAR_LENGTH;
+      barHeight = map(height, 0, TOP, 0, centerBar);
+      for (int i=0;i<centerBar;i++) {
+        uint32_t color = 0;
+        if (barHeight >= i) {
+          color = Wheel(height);
+        }
+        strip.setPixelColor(BAR_LENGTH + i, color);
+        strip.setPixelColor(strip.numPixels() - BAR_LENGTH - i, color);
+      }
+      strip.show();
+      break;
+    } 
+    
+    case modeRainbow:
+      strip.setBrightness(TOP);
+      rainbowCycle(20);
+      break;
+      
+    case modeTheaterChaseRainbow:
+      strip.setBrightness(TOP);
+      theaterChaseRainbow(50);
+      break;
   }
-//  colorWipe(strip.Color(255, 255, 255), 0);
-  rainbowCycleStep(currentCycle);
 }
 
 // Fill the dots one after the other with a color
@@ -102,24 +156,6 @@ void rainbowCycleStep(uint16_t j) {
     strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
   }
   strip.show();
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
 }
 
 //Theatre-style crawling lights with rainbow effect
