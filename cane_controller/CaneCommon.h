7898c556c8ef846ec1f2611230dc7eb107eca2ed
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2016-04-11 11:01:36 -0700

    Cleaning up header.

--- cane_controller/CaneCommon.h
+++ cane_controller/CaneCommon.h
@@ -18,13 +18,7 @@
 
 #define NUMBER_OF_READINGS  8 // number of readings to run average from.
 
-extern float minReadings[NUMBER_OF_AXIS];
-extern float maxReadings[NUMBER_OF_AXIS];
-extern float readings[NUMBER_OF_AXIS];
-extern float readingSums[NUMBER_OF_AXIS];
-extern float sreadings[NUMBER_OF_AXIS][NUMBER_OF_READINGS];
 extern float currentX;
-
 extern Adafruit_NeoPixel strip;
 
 #endif
