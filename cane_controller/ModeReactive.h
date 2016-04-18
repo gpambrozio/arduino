35247e3c54a7a416f3088df99700fddb44490194
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2016-04-17 20:36:03 -0700

    Reactive mode now has 2 dots that chase each other and have an effect when they meet

--- cane_controller/ModeReactive.h
+++ cane_controller/ModeReactive.h
@@ -4,30 +4,38 @@
 #include "Mode.h"
 #include "CaneCommon.h"
 
-#define ZERO_LEDS  15
+#define ZERO_LEDS  12
+#define DOTS_SCALE  3
+#define DOT_ENCOUNTER_RIPPLE 12
 
 class ModeReactive : public Mode
 {
   public:
     using Mode::Mode;
-    virtual void init() { cycleIndex = 0; fallingDot = 0; dotRising = false; }
+    virtual void init() { cycleIndex = 0; fallingDot[0] = 0; dotRising[0] = false; fallingDot[0] = LEDS; dotRising[1] = false; }
     virtual bool step(unsigned long dt) {
-      int lights = map(abs(currentX), 0, ONE_G, 0, LEDS - ZERO_LEDS);
-      for (uint16_t i=0; i<LEDS; i++) {
-        if (i < lights && i != fallingDot/3) {
-          strip.setPixelColor(i, 0x0);
-        } else {
+      int lights = (int)((currentX < 0 ? -currentX : currentX) * (LEDS - ZERO_LEDS) / ONE_G);
+      for (int16_t i=0; i<LEDS; i++) {
+        int distance = min(abs(fallingDot[0]/DOTS_SCALE - i), abs(fallingDot[1]/DOTS_SCALE - i));
+        if (i >= lights || distance == 0) {
           strip.setPixelColor(i, wheel(((cycleIndex/2)+i) & 0xFF));
+        } else if (abs(fallingDot[0] - fallingDot[1]) <= 1 && distance <= DOT_ENCOUNTER_RIPPLE) {
+          float brightness = ((float)map(distance, 0, DOT_ENCOUNTER_RIPPLE, 255, 40) / 255.0);
+          strip.setPixelColor(i, wheel(((cycleIndex/2)+i) & 0xFF, brightness));
+        } else {
+          strip.setPixelColor(i, 0x0);
         }
       }
-      if (dotRising) {
-        if (--fallingDot < 0) {
-          fallingDot = 0;
-          dotRising = false;
+      for (uint16_t i=0; i<2; i++) {
+        if (dotRising[i]) {
+          if (--fallingDot[i] < 0) {
+            fallingDot[i] = 0;
+            dotRising[i] = false;
+          }
+        } else if (++fallingDot[i] >= lights*DOTS_SCALE) {
+          fallingDot[i]--;
+          dotRising[i] = true;
         }
-      } else if (++fallingDot >= lights*3) {
-        fallingDot--;
-        dotRising = true;
       }
       if (++cycleIndex >= 256*2) {
         cycleIndex = 0;
@@ -37,8 +45,8 @@ class ModeReactive : public Mode
     }
   private:
     int cycleIndex;
-    int fallingDot;
-    boolean dotRising;
+    int fallingDot[2];
+    boolean dotRising[2];
 };
 
 #endif
