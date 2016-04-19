b6dfc3a88e281b3360e45af1c8dd61a8530940a0
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2016-04-19 12:59:37 -0700

    Faster math!

--- cane_controller/Mode.cpp
+++ cane_controller/Mode.cpp
@@ -10,20 +10,29 @@ Mode::Mode() {
 uint32_t Mode::wheel(byte WheelPos, int brightness) {
   WheelPos = 255 - WheelPos;
   if (WheelPos < 85) {
-   return strip.Color(brightness * (255 - WheelPos * 3) / 255, 0, brightness * WheelPos * 3 / 255);
+   return strip.Color((brightness * (255 - WheelPos * 3)) >> 8, 0, (brightness * WheelPos * 3) >> 8);
   } else if(WheelPos < 170) {
     WheelPos -= 85;
-   return strip.Color(0, brightness * WheelPos * 3 / 255, brightness * (255 - WheelPos * 3) / 255);
+   return strip.Color(0, (brightness * WheelPos * 3) >> 8, (brightness * (255 - WheelPos * 3)) >> 8);
   } else {
    WheelPos -= 170;
-   return strip.Color(brightness * WheelPos * 3 / 255, brightness * (255 - WheelPos * 3) / 255, 0);
+   return strip.Color((brightness * WheelPos * 3) >> 8, (brightness * (255 - WheelPos * 3)) >> 8, 0);
   }
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Mode::wheel(byte WheelPos) {
-  return wheel(WheelPos, 255);
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
 }
 
 void Mode::init() {
