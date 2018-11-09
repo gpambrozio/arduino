409045f3c3fe7c867adc633b7c6093f56f4e034e
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-09 13:16:37 -1000

    First versions of 2 new sketches

---
+++ agnes_fan/agnes_fan.ino
@@ -0,0 +1,157 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <Bounce2.h>
+
+#define MOTOR_UP   12
+#define MOTOR_DOWN 14
+
+#define KEY_UP     4
+#define KEY_DOWN   5
+
+#define TOTAL_MOVE_TIME   6000
+
+#define WLAN_AGNES
+
+#include "Passwords.h"
+
+ESP8266WebServer server(80);
+
+Bounce debouncerDown = Bounce(); 
+Bounce debouncerUp = Bounce(); 
+
+int goToPosition = 0;
+int currentPosition = 0;
+
+void setup() {
+  pinMode(KEY_UP, INPUT_PULLUP);
+  pinMode(KEY_DOWN, INPUT_PULLUP);
+
+  debouncerDown.attach(KEY_DOWN);
+  debouncerDown.interval(50); // interval in ms
+
+  debouncerUp.attach(KEY_UP);
+  debouncerUp.interval(50); // interval in ms
+
+  pinMode(MOTOR_UP, INPUT);
+  pinMode(MOTOR_DOWN, INPUT);
+  digitalWrite(MOTOR_UP, LOW);
+  digitalWrite(MOTOR_DOWN, LOW);
+  
+  Serial.begin(115200);
+  Serial.println("Starting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Connect Failed! Rebooting...");
+    delay(1000);
+    ESP.restart();
+  }
+
+  if (MDNS.begin("fan")) {
+    Serial.println("MDNS responder started");
+  }
+
+  ArduinoOTA.setPort(8266);
+  ArduinoOTA.setHostname("fan");
+  ArduinoOTA.begin();
+
+  server.on("/", []() {
+    if (!server.authenticate(WLAN_SSID, WLAN_PASS)) {
+      return server.requestAuthentication();
+    }
+    server.send(200, "text/plain", "Login OK");
+  });
+  server.on("/move", []() {
+    if (!server.authenticate(WLAN_SSID, WLAN_PASS)) {
+      return server.requestAuthentication();
+    }
+    int pos = parseInt(server.arg("pos").c_str());
+    goToPosition = pos;
+    server.send(200, "text/plain", "Move OK");
+  });
+  server.begin();
+
+  Serial.print("Open http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/ in your browser to see it working");
+}
+
+bool goingUp = false;
+bool goingDown = false;
+long startMove;
+
+void loop() {
+  debouncerDown.update();
+  debouncerUp.update();
+
+  if (currentPosition != goToPosition) {
+    long moveTime = (goToPosition < 0 ? 1000 : 0) + TOTAL_MOVE_TIME * abs(currentPosition - max(0, goToPosition)) / 100;
+    bool goingUp = goToPosition > currentPosition;
+    Serial.print("Moving ");
+    Serial.print(goingUp ? "up for " : "down for ");
+    Serial.println(moveTime);
+
+    if (goingUp) {
+      digitalWrite(MOTOR_UP, HIGH);
+      pinMode(MOTOR_UP, OUTPUT);
+    } else {
+      digitalWrite(MOTOR_DOWN, HIGH);
+      pinMode(MOTOR_DOWN, OUTPUT);
+    }
+    delay(moveTime);
+    pinMode(MOTOR_UP, INPUT);
+    pinMode(MOTOR_DOWN, INPUT);
+
+    currentPosition = max(0, goToPosition);
+    goToPosition = currentPosition;
+  }
+  
+  bool goUp = debouncerUp.read() == LOW;
+  if (goingUp != goUp) {
+    goingUp = goUp;
+    if (goUp) {
+      startMove = millis();
+      digitalWrite(MOTOR_UP, HIGH);
+      pinMode(MOTOR_UP, OUTPUT);
+    } else {
+      pinMode(MOTOR_UP, INPUT);
+      long moveTime = millis() - startMove;
+      currentPosition = min((long)100, currentPosition + 100 * moveTime / TOTAL_MOVE_TIME);
+      goToPosition = currentPosition;
+    }
+    Serial.println("Going up changed");
+  }
+  bool goDown = debouncerDown.read() == LOW;
+  if (goingDown != goDown) {
+    goingDown = goDown;
+    if (goDown) {
+      startMove = millis();
+      digitalWrite(MOTOR_DOWN, HIGH);
+      pinMode(MOTOR_DOWN, OUTPUT);
+    } else {
+      pinMode(MOTOR_DOWN, INPUT);
+      long moveTime = millis() - startMove;
+      currentPosition = max((long)0, currentPosition - 100 * moveTime / TOTAL_MOVE_TIME);
+      goToPosition = currentPosition;
+    }
+    Serial.println("Going down changed");
+  }
+    
+  ArduinoOTA.handle();
+  server.handleClient();
+}
+
+int parseInt(const char *buffer) {
+  int ret = 0;
+  bool isNegative = false;
+  if (*buffer == '-') {
+    isNegative = true;
+    buffer++;
+  }
+  while (*buffer >= '0' && *buffer <= '9') {
+    ret = ret * 10 + (*buffer++ - '0');
+  }
+  return isNegative? -ret : ret;
+}
---
+++ agnes_keypad/agnes_keypad.ino
@@ -0,0 +1,199 @@
+/*************************************************** 
+  This is a test example for the Adafruit Trellis w/HT16K33
+
+  Designed specifically to work with the Adafruit Trellis 
+  ----> https://www.adafruit.com/products/1616
+  ----> https://www.adafruit.com/products/1611
+
+  These displays use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include "Adafruit_Trellis.h"
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+
+#define WLAN_AGNES
+
+#include <Passwords.h>
+
+ESP8266WebServer server(80);
+HTTPClient http;
+
+/*************************************************** 
+  This example shows reading buttons and setting/clearing buttons in a loop
+  "momentary" mode has the LED light up only when a button is pressed
+  "latching" mode lets you turn the LED on/off when pressed
+
+  Up to 8 matrices can be used but this example will show 4 or 1
+ ****************************************************/
+
+Adafruit_Trellis matrix0 = Adafruit_Trellis();
+
+// Just one
+Adafruit_TrellisSet trellis =  Adafruit_TrellisSet(&matrix0);
+// or use the below to select 4, up to 8 can be passed in
+//Adafruit_TrellisSet trellis =  Adafruit_TrellisSet(&matrix0, &matrix1, &matrix2, &matrix3);
+
+// set to however many you're working with here, up to 8
+#define NUMTRELLIS 1
+
+#define numKeys (NUMTRELLIS * 16)
+
+// Connect Trellis Vin to 5V and Ground to ground.
+#define INTPIN 2
+// Connect I2C SDA pin to your Arduino SDA line
+// Connect I2C SCL pin to your Arduino SCL line
+// All Trellises share the SDA, SCL and INT pin! 
+// Even 8 tiles use only 3 wires max
+
+// TTF Config
+// ==========
+// For the breakout, you can use any 2 or 3 pins
+// These pins will also work for the 1.8" TFT shield
+#define TFT_CS     15
+#define TFT_RST    0   // you can also connect this to the Arduino reset
+                       // in which case, set this #define pin to -1!
+#define TFT_DC     16
+
+// Option 1 (recommended): must use the hardware SPI pins
+// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+
+// For 1.44" and 1.8" TFT with ST7735 use
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Trellis Demo");
+
+  // INT pin requires a pullup
+  pinMode(INTPIN, INPUT_PULLUP);
+  digitalWrite(INTPIN, HIGH);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Connect Failed! Rebooting...");
+    delay(1000);
+    ESP.restart();
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("keypad")) {
+    Serial.println("MDNS responder started");
+  }
+
+  ArduinoOTA.setPort(8266);
+  ArduinoOTA.setHostname("keypad");
+  ArduinoOTA.begin();
+
+  server.on("/", []() {
+    if (!server.authenticate(WLAN_SSID, WLAN_PASS)) {
+      return server.requestAuthentication();
+    }
+    server.send(200, "text/plain", "Login OK");
+  });
+  server.begin();
+
+  // begin() with the addresses of each panel in order
+  // I find it easiest if the addresses are in order
+  trellis.begin(0x70);  // only one
+
+  // light up all the LEDs in order
+  for (uint8_t i=0; i<numKeys; i++) {
+    trellis.setLED(i);
+    trellis.writeDisplay();    
+    delay(50);
+  }
+  // then turn them off
+  for (uint8_t i=0; i<numKeys; i++) {
+    trellis.clrLED(i);
+    trellis.writeDisplay();    
+    delay(50);
+  }
+
+  tft.initR(INITR_144GREENTAB);   // initialize a ST7735S chip, black tab
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+}
+
+long nextTFTUpdate = 0;
+
+void loop() {
+  delay(30); // 30ms delay is required, dont remove me!
+  
+  // If a button was just pressed or released...
+  if (trellis.readSwitches()) {
+    // go through every button
+    for (uint8_t i=0; i<numKeys; i++) {
+      // if it was pressed...
+      if (trellis.justPressed(i)) {
+        Serial.print("v"); Serial.println(i);
+        // Alternate the LED
+        if (trellis.isLED(i)) {
+          trellis.clrLED(i);
+        } else {
+          trellis.setLED(i);
+        }
+        
+        if (i % 2 == 0) {
+          http.begin("http://agnespanel.local:8080/text//0");
+        } else {
+          http.begin("http://agnespanel.local:8080/image/thanks/1");
+        }
+        
+        int httpCode = http.GET();
+        if (httpCode > 0) {
+          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+        } else {
+          Serial.print("[HTTP] GET... failed, error: "); Serial.println(http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+      }
+    }
+    // tell the trellis to set the LEDs we requested
+    trellis.writeDisplay();
+  }
+  ArduinoOTA.handle();
+  server.handleClient();
+  if (millis() > nextTFTUpdate) {
+    nextTFTUpdate = millis() + 1000;
+    tftPrintTest();
+  }
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.startWrite();
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA, ST77XX_BLACK);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  tft.print(" s.");
+  tft.endWrite();
+}
