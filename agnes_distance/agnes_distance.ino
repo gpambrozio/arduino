4c1a8b57187a93deb4e2671e3f2e3136b0333297
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-28 13:38:07 -1000

    Adding bluetooth

--- agnes_distance/agnes_distance.ino
+++ agnes_distance/agnes_distance.ino
@@ -6,18 +6,52 @@
  *
  */
 
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 // defines pins numbers
-#define trigPin  9
-#define echoPin  10
+#define trigPin  26
+#define echoPin  25
 
 // defines variables
 long duration;
-int distance;
+float distance = 0;
+
+BLEServer *pServer;
+BLEService *pService;
+BLECharacteristic *pCharacteristic;
+BLEAdvertising *pAdvertising;
 
 void setup() {
+  
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(115200); // Starts the serial communication
+
+  BLEDevice::init("AgnesBehinds");
+  pServer = BLEDevice::createServer();
+  pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  pCharacteristic->setValue(distance);
+  pService->start();
+  pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
 }
 
 void loop() {
@@ -31,12 +65,16 @@ void loop() {
   digitalWrite(trigPin, LOW);
   
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH, 10000);
   
   // Calculating the distance
-  distance= duration*0.017;
-  
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  distance = (float)duration * 0.017;
+
+  if (duration > 20) {
+    // Prints the distance on the Serial Monitor
+    pCharacteristic->setValue(distance);
+    pCharacteristic->notify();
+    Serial.print("Distance: ");
+    Serial.println(distance);
+  }
 }
