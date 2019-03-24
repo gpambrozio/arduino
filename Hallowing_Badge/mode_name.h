004835f6a075f630688cd305b26b9fa1deadbc3c
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2019-03-24 00:04:34 -0700

    Smaller image and smarter padding

--- Hallowing_Badge/mode_name.h
+++ Hallowing_Badge/mode_name.h
@@ -11,10 +11,11 @@ class ModeName : public Mode
     virtual void init() {}
     virtual void setup() {
       column = -128;
+      tft.fillScreen(0xffff);
     }
     virtual void tearDown() {}
     virtual void draw(bool hasClick) {
-      setAddrWindow(0, 0, 128, 128);
+      setAddrWindow(0, (128 - NAME_ROWS) / 2, 128, NAME_ROWS);
 
       uint16_t *dmaPtr;   // Pointer into DMA output buffer (16 bits/pixel)
       uint8_t   col, row; // X,Y pixel counters
@@ -25,8 +26,6 @@ class ModeName : public Mode
       uint16_t *srcPtr;
 
       // Process rows
-      uint16_t padding = (128 - NAME_ROWS) / 2;
-      pad(padding, nBytes);
       for (row=0; row<NAME_ROWS; row++) {
         dmaPtr  = &dmaBuf[dmaIdx][0];
 
@@ -44,7 +43,6 @@ class ModeName : public Mode
         }
         dmaXfer(nBytes);
       }
-      pad(padding, nBytes);
       if (++column >= NAME_COLS) {
         column = -128;
       }
@@ -53,16 +51,6 @@ class ModeName : public Mode
   
   private:
     int column;
-    void pad(uint16_t padding, uint16_t  nBytes) {
-      uint16_t *dmaPtr;
-      for (int row=0; row<padding; row++) {
-        dmaPtr = &dmaBuf[dmaIdx][0];
-        for (int col=0; col<128; col++) {
-          *dmaPtr++ = 0xffff;
-        }
-        dmaXfer(nBytes);
-      }
-    }
 };
 
 #endif
