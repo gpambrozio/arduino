96b262119d370956b3ff972f661225b9d108c64a
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2019-05-09 22:51:03 -0700

    Fixing compilation issue

--- agnes_distance/agnes_distance.ino
+++ agnes_distance/agnes_distance.ino
@@ -72,7 +72,7 @@ void loop() {
 
 // From https://github.com/TFmini/TFmini-Arduino#tfmini_arduino_hardware_serialpolling
 void getTFminiData(int* distance, int* strength) {
-  static char i = 0;
+  static uint8_t i = 0;
   static int rx[9];
   if (SerialTFMini.available()) {  
     rx[i] = SerialTFMini.read();
@@ -82,7 +82,7 @@ void getTFminiData(int* distance, int* strength) {
       i = 0;
     } else if (i == 8) {
       int checksum = 0;
-      for (char j = 0; j < 8; j++) {
+      for (uint8_t j = 0; j < 8; j++) {
         checksum += rx[j];
       }
       if (rx[8] == (checksum & 0xFF)) {
