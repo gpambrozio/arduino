7f9e8e4bff2bec9c4fe13451c571da7bb282543b
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2020-12-11 21:03:26 -1000

    library update

--- libraries/RF24/utility/SPIDEV/spi.cpp
+++ libraries/RF24/utility/SPIDEV/spi.cpp
@@ -23,16 +23,15 @@
 #define RF24_SPIDEV_BITS 8
 
 SPI::SPI()
-        :fd(-1), _spi_speed(RF24_SPIDEV_SPEED)
+        :fd(-1), _spi_speed(RF24_SPI_SPEED)
 {
 }
 
-bool spiIsInitialized = 0;
 
 void SPI::begin(int busNo, uint32_t spi_speed)
 {
 
-    if (spiIsInitialized) {
+    if (this->spiIsInitialized) {
         return;
     }
 
@@ -61,7 +60,7 @@ void SPI::begin(int busNo, uint32_t spi_speed)
         abort();
 
   }*/
-    spiIsInitialized = true;
+    this->spiIsInitialized = true;
     init(spi_speed);
 }
 
@@ -143,7 +142,7 @@ uint8_t SPI::transfer(uint8_t tx)
     uint8_t rx;
     tr.rx_buf = (unsigned long) &rx;
     tr.len = sizeof(tx);
-    tr.speed_hz = _spi_speed; //RF24_SPIDEV_SPEED;
+    tr.speed_hz = _spi_speed; //RF24_SPI_SPEED;
     tr.delay_usecs = 0;
     tr.bits_per_word = RF24_SPIDEV_BITS;
     tr.cs_change = 0;
@@ -168,7 +167,7 @@ void SPI::transfernb(char* tbuf, char* rbuf, uint32_t len)
     tr.tx_buf = (unsigned long) tbuf;
     tr.rx_buf = (unsigned long) rbuf;
     tr.len = len;
-    tr.speed_hz = _spi_speed; //RF24_SPIDEV_SPEED;
+    tr.speed_hz = _spi_speed; //RF24_SPI_SPEED;
     tr.delay_usecs = 0;
     tr.bits_per_word = RF24_SPIDEV_BITS;
     tr.cs_change = 0;
